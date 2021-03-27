9edb35a514e80d2075d55e1829b8ac84dae944d2
zhaoshaoyong <zhaoshaoyong@sfmail.sf-express.com>
2021-03-27 13:20:42 +0800

    add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const app = new Koa()
 const Router = require('koa-router')
 const koaBody = require('koa-body');
+const cors = require('@koa/cors');
 const {ProductModel} = require('./db.js')
 var router = new Router() 
 router.prefix('/payment')
@@ -25,6 +26,28 @@ router.post('/submit',async function(ctx,next){
     ctx.body = res
     
 })
+
+router.get('/testAjax',async function(ctx,next){
+    await new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            resolve()
+        },3000)
+    })
+    ctx.body = 'testAjax'
+
+    // setTimeout(function(){
+    //     ctx.body = 'testAjax'
+    // },3000)
+})
+router.post('/testAjax',async function(ctx,next){
+    await new Promise((resolve,reject)=>{
+        setTimeout(function(){
+            resolve()
+        },3000)
+    })
+    ctx.body = 'testAjax'
+})
+app.use(cors());
 // multipart 支持formdata
 app.use(koaBody({multipart: true}));
 app.use(router.routes())
